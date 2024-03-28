14ae6e3b99e18bf803e3cadb3e075837afd4b63e
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-03-28 11:18:01 +0530

    Update js.js

--- trainers/js.js
+++ trainers/js.js
@@ -2,7 +2,7 @@
 let i = 1;
 const data = [
  // add data here
-[i++,"Ankita","","./trainers/Ankita/1.bmp","./trainers/Ankita/2.bmp","./trainers/Ankita/3.bmp","./trainers/Ankita/4.bmp","./trainers/Ankita/5.bmp"],
+[i++,"Ankita","57334538","./trainers/Ankita/1.bmp","./trainers/Ankita/2.bmp","./trainers/Ankita/3.bmp","./trainers/Ankita/4.bmp","./trainers/Ankita/5.bmp"],
 [i++,"Pinki","","./trainers/Pinki/1.bmp","./trainers/Pinki/2.bmp","./trainers/Pinki/3.bmp","./trainers/Pinki/4.bmp","./trainers/Pinki/5.bmp"],
          // 1-30 are done....
 // adding more rows..
