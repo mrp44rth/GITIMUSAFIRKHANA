d2da3ee82b1cc88159df5768943ff50caea4bc07
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-04-08 07:11:02 +0530

    Add files via upload

---
+++ new portal/FAP1 (2489769)/js.js
@@ -0,0 +1,136 @@
+let i=1;
+const data = [
+[i++, "Anchal Sahu", "72776068", "./data/AnchalSahu_72776068/1.bmp", "./data/AnchalSahu_72776068/2.bmp", "./data/AnchalSahu_72776068/3.bmp", "./data/AnchalSahu_72776068/4.bmp", "./data/AnchalSahu_72776068/5.bmp"],
+[i++, "Anjali", "93025657", "./data/Anjali_93025657/1.bmp", "./data/Anjali_93025657/2.bmp", "./data/Anjali_93025657/3.bmp", "./data/Anjali_93025657/4.bmp", "./data/Anjali_93025657/5.bmp"],
+[i++, "Anshika", "22439737", "./data/Anshika_22439737/1.bmp", "./data/Anshika_22439737/2.bmp", "./data/Anshika_22439737/3.bmp", "./data/Anshika_22439737/4.bmp", "./data/Anshika_22439737/5.bmp"],
+[i++, "Anshika", "24974165", "./data/Anshika_24974165/1.bmp", "./data/Anshika_24974165/2.bmp", "./data/Anshika_24974165/3.bmp", "./data/Anshika_24974165/4.bmp", "./data/Anshika_24974165/5.bmp"],
+[i++, "Aradhya Yadav", "42534413", "./data/AradhyaYadav_42534413/1.bmp", "./data/AradhyaYadav_42534413/2.bmp", "./data/AradhyaYadav_42534413/3.bmp", "./data/AradhyaYadav_42534413/4.bmp", "./data/AradhyaYadav_42534413/5.bmp"],
+[i++, "AYUSHI", "26004570", "./data/AYUSHI_26004570/1.bmp", "./data/AYUSHI_26004570/2.bmp", "./data/AYUSHI_26004570/3.bmp", "./data/AYUSHI_26004570/4.bmp", "./data/AYUSHI_26004570/5.bmp"],
+[i++, "Divyanshi", "20408289", "./data/Divyanshi_20408289/1.bmp", "./data/Divyanshi_20408289/2.bmp", "./data/Divyanshi_20408289/3.bmp", "./data/Divyanshi_20408289/4.bmp", "./data/Divyanshi_20408289/5.bmp"],
+[i++, "Gunjan", "34485903", "./data/Gunjan_34485903/1.bmp", "./data/Gunjan_34485903/2.bmp", "./data/Gunjan_34485903/3.bmp", "./data/Gunjan_34485903/4.bmp", "./data/Gunjan_34485903/5.bmp"],
+[i++, "KHUSHBU VERMA", "82451623", "./data/KHUSHBUVERMA_82451623/1.bmp", "./data/KHUSHBUVERMA_82451623/2.bmp", "./data/KHUSHBUVERMA_82451623/3.bmp", "./data/KHUSHBUVERMA_82451623/4.bmp", "./data/KHUSHBUVERMA_82451623/5.bmp"],
+[i++, "Khushi", "84654326", "./data/Khushi_84654326/1.bmp", "./data/Khushi_84654326/2.bmp", "./data/Khushi_84654326/3.bmp", "./data/Khushi_84654326/4.bmp", "./data/Khushi_84654326/5.bmp"],
+[i++, "KOMAL", "29619490", "./data/KOMAL_29619490/1.bmp", "./data/KOMAL_29619490/2.bmp", "./data/KOMAL_29619490/3.bmp", "./data/KOMAL_29619490/4.bmp", "./data/KOMAL_29619490/5.bmp"],
+[i++, "Kshama Tiwari", "43937206", "./data/KshamaTiwari_43937206/1.bmp", "./data/KshamaTiwari_43937206/2.bmp", "./data/KshamaTiwari_43937206/3.bmp", "./data/KshamaTiwari_43937206/4.bmp", "./data/KshamaTiwari_43937206/5.bmp"],
+[i++, "LALTI", "18845914", "./data/LALTI_18845914/1.bmp", "./data/LALTI_18845914/2.bmp", "./data/LALTI_18845914/3.bmp", "./data/LALTI_18845914/4.bmp", "./data/LALTI_18845914/5.bmp"],
+[i++, "Mahak", "50078452", "./data/Mahak_50078452/1.bmp", "./data/Mahak_50078452/2.bmp", "./data/Mahak_50078452/3.bmp", "./data/Mahak_50078452/4.bmp", "./data/Mahak_50078452/5.bmp"],
+[i++, "Manisha", "22333363", "./data/Manisha_22333363/1.bmp", "./data/Manisha_22333363/2.bmp", "./data/Manisha_22333363/3.bmp", "./data/Manisha_22333363/4.bmp", "./data/Manisha_22333363/5.bmp"],
+[i++, "Manisha Yadav", "64612154", "./data/ManishaYadav_64612154/1.bmp", "./data/ManishaYadav_64612154/2.bmp", "./data/ManishaYadav_64612154/3.bmp", "./data/ManishaYadav_64612154/4.bmp", "./data/ManishaYadav_64612154/5.bmp"],
+[i++, "MANJU", "11176108", "./data/MANJU_11176108/1.bmp", "./data/MANJU_11176108/2.bmp", "./data/MANJU_11176108/3.bmp", "./data/MANJU_11176108/4.bmp", "./data/MANJU_11176108/5.bmp"],
+[i++, "Nainci", "93146325", "./data/Nainci_93146325/1.bmp", "./data/Nainci_93146325/2.bmp", "./data/Nainci_93146325/3.bmp", "./data/Nainci_93146325/4.bmp", "./data/Nainci_93146325/5.bmp"],
+[i++, "Nainsi Kumari", "68415347", "./data/NainsiKumari_68415347/1.bmp", "./data/NainsiKumari_68415347/2.bmp", "./data/NainsiKumari_68415347/3.bmp", "./data/NainsiKumari_68415347/4.bmp", "./data/NainsiKumari_68415347/5.bmp"],
+[i++, "Nandini", "66347788", "./data/Nandini_66347788/1.bmp", "./data/Nandini_66347788/2.bmp", "./data/Nandini_66347788/3.bmp", "./data/Nandini_66347788/4.bmp", "./data/Nandini_66347788/5.bmp"],
+[i++, "Nirali Yadav", "62722542", "./data/NiraliYadav_62722542/1.bmp", "./data/NiraliYadav_62722542/2.bmp", "./data/NiraliYadav_62722542/3.bmp", "./data/NiraliYadav_62722542/4.bmp", "./data/NiraliYadav_62722542/5.bmp"],
+[i++, "Pinki", "52559123", "./data/Pinki_52559123/1.bmp", "./data/Pinki_52559123/2.bmp", "./data/Pinki_52559123/3.bmp", "./data/Pinki_52559123/4.bmp", "./data/Pinki_52559123/5.bmp"],
+[i++, "Poonam", "37494608", "./data/Poonam_37494608/1.bmp", "./data/Poonam_37494608/2.bmp", "./data/Poonam_37494608/3.bmp", "./data/Poonam_37494608/4.bmp", "./data/Poonam_37494608/5.bmp"],
+[i++, "Poonam Yadav", "55234489", "./data/PoonamYadav_55234489/1.bmp", "./data/PoonamYadav_55234489/2.bmp", "./data/PoonamYadav_55234489/3.bmp", "./data/PoonamYadav_55234489/4.bmp", "./data/PoonamYadav_55234489/5.bmp"],
+[i++, "Pritee Devi", "88239824", "./data/PriteeDevi_88239824/1.bmp", "./data/PriteeDevi_88239824/2.bmp", "./data/PriteeDevi_88239824/3.bmp", "./data/PriteeDevi_88239824/4.bmp", "./data/PriteeDevi_88239824/5.bmp"],
+[i++, "Priyanka Nishad", "14187135", "./data/PriyankaNishad_14187135/1.bmp", "./data/PriyankaNishad_14187135/2.bmp", "./data/PriyankaNishad_14187135/3.bmp", "./data/PriyankaNishad_14187135/4.bmp", "./data/PriyankaNishad_14187135/5.bmp"],
+[i++, "Ragini", "33525035", "./data/Ragini_33525035/1.bmp", "./data/Ragini_33525035/2.bmp", "./data/Ragini_33525035/3.bmp", "./data/Ragini_33525035/4.bmp", "./data/Ragini_33525035/5.bmp"],
+[i++, "Ragini Kumari", "14903146", "./data/RaginiKumari_14903146/1.bmp", "./data/RaginiKumari_14903146/2.bmp", "./data/RaginiKumari_14903146/3.bmp", "./data/RaginiKumari_14903146/4.bmp", "./data/RaginiKumari_14903146/5.bmp"],
+[i++, "Reecha", "50165912", "./data/Reecha_50165912/1.bmp", "./data/Reecha_50165912/2.bmp", "./data/Reecha_50165912/3.bmp", "./data/Reecha_50165912/4.bmp", "./data/Reecha_50165912/5.bmp"],
+[i++, "Renu", "67979285", "./data/Renu_67979285/1.bmp", "./data/Renu_67979285/2.bmp", "./data/Renu_67979285/3.bmp", "./data/Renu_67979285/4.bmp", "./data/Renu_67979285/5.bmp"],
+[i++, "RENU", "99897080", "./data/RENU_99897080/1.bmp", "./data/RENU_99897080/2.bmp", "./data/RENU_99897080/3.bmp", "./data/RENU_99897080/4.bmp", "./data/RENU_99897080/5.bmp"],
+[i++, "Ritika", "95323041", "./data/Ritika_95323041/1.bmp", "./data/Ritika_95323041/2.bmp", "./data/Ritika_95323041/3.bmp", "./data/Ritika_95323041/4.bmp", "./data/Ritika_95323041/5.bmp"],
+[i++, "Roli", "79180699", "./data/Roli_79180699/1.bmp", "./data/Roli_79180699/2.bmp", "./data/Roli_79180699/3.bmp", "./data/Roli_79180699/4.bmp", "./data/Roli_79180699/5.bmp"],
+[i++, "Romi Maurya", "31001405", "./data/RomiMaurya_31001405/1.bmp", "./data/RomiMaurya_31001405/2.bmp", "./data/RomiMaurya_31001405/3.bmp", "./data/RomiMaurya_31001405/4.bmp", "./data/RomiMaurya_31001405/5.bmp"],
+[i++, "Rubi", "8927118", "./data/Rubi_8927118/1.bmp", "./data/Rubi_8927118/2.bmp", "./data/Rubi_8927118/3.bmp", "./data/Rubi_8927118/4.bmp", "./data/Rubi_8927118/5.bmp"],
+[i++, "RUBI", "96144178", "./data/RUBI_96144178/1.bmp", "./data/RUBI_96144178/2.bmp", "./data/RUBI_96144178/3.bmp", "./data/RUBI_96144178/4.bmp", "./data/RUBI_96144178/5.bmp"],
+[i++, "Sadhna", "68326145", "./data/Sadhna_68326145/1.bmp", "./data/Sadhna_68326145/2.bmp", "./data/Sadhna_68326145/3.bmp", "./data/Sadhna_68326145/4.bmp", "./data/Sadhna_68326145/5.bmp"],
+[i++, "SANTOSH KUMARI", "64785151", "./data/SANTOSHKUMARI_64785151/1.bmp", "./data/SANTOSHKUMARI_64785151/2.bmp", "./data/SANTOSHKUMARI_64785151/3.bmp", "./data/SANTOSHKUMARI_64785151/4.bmp", "./data/SANTOSHKUMARI_64785151/5.bmp"],
+[i++, "Saraswati", "87388799", "./data/Saraswati_87388799/1.bmp", "./data/Saraswati_87388799/2.bmp", "./data/Saraswati_87388799/3.bmp", "./data/Saraswati_87388799/4.bmp", "./data/Saraswati_87388799/5.bmp"],
+[i++, "SARITA", "36454682", "./data/SARITA_36454682/1.bmp", "./data/SARITA_36454682/2.bmp", "./data/SARITA_36454682/3.bmp", "./data/SARITA_36454682/4.bmp", "./data/SARITA_36454682/5.bmp"],
+[i++, "SEEMA", "92320640", "./data/SEEMA_92320640/1.bmp", "./data/SEEMA_92320640/2.bmp", "./data/SEEMA_92320640/3.bmp", "./data/SEEMA_92320640/4.bmp", "./data/SEEMA_92320640/5.bmp"],
+[i++, "SHILPA", "62539433", "./data/SHILPA_62539433/1.bmp", "./data/SHILPA_62539433/2.bmp", "./data/SHILPA_62539433/3.bmp", "./data/SHILPA_62539433/4.bmp", "./data/SHILPA_62539433/5.bmp"],
+[i++, "Shivani", "56588434", "./data/Shivani_56588434/1.bmp", "./data/Shivani_56588434/2.bmp", "./data/Shivani_56588434/3.bmp", "./data/Shivani_56588434/4.bmp", "./data/Shivani_56588434/5.bmp"],
+[i++, "Shivanshi", "91526267", "./data/Shivanshi_91526267/1.bmp", "./data/Shivanshi_91526267/2.bmp", "./data/Shivanshi_91526267/3.bmp", "./data/Shivanshi_91526267/4.bmp", "./data/Shivanshi_91526267/5.bmp"],
+[i++, "Sonam", "86453089", "./data/Sonam_86453089/1.bmp", "./data/Sonam_86453089/2.bmp", "./data/Sonam_86453089/3.bmp", "./data/Sonam_86453089/4.bmp", "./data/Sonam_86453089/5.bmp"],
+[i++, "Sunita Yadav", "27158104", "./data/SunitaYadav_27158104/1.bmp", "./data/SunitaYadav_27158104/2.bmp", "./data/SunitaYadav_27158104/3.bmp", "./data/SunitaYadav_27158104/4.bmp", "./data/SunitaYadav_27158104/5.bmp"],
+
+];
+
+let currentSortColumn = -1; // Initially, no column is sorted
+let isAscending = true; // Initially, sorting is in ascending order
+
+document.addEventListener('DOMContentLoaded', function () {
+    renderTable(data);
+    
+    const searchInput = document.getElementById('search');
+    searchInput.addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase();
+        const filteredData = data.filter(row => row.some(cell => cell.toString().toLowerCase().includes(searchTerm)));
+        renderTable(filteredData);
+    });
+
+    const contrastRange = document.getElementById('contrast');
+    contrastRange.addEventListener('input', function () {
+        const contrastValue = this.value;
+        document.querySelector('.contrast-value').textContent = `${contrastValue}%`;
+        updateImageContrast(contrastValue);
+    });
+});
+
+function renderTable(data) {
+    const tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = '';
+
+    data.forEach(row => {
+        const tr = document.createElement('tr');
+
+        row.forEach((cell, index) => {
+            const td = document.createElement('td');
+
+            if (index >= 3 && index <= 7) {
+                const img = document.createElement('img');
+                img.src = cell;
+                img.alt = `Finger ${index - 1}`;
+                img.style.width = '100px'; // Set width to auto for actual size
+                img.style.height = '100px'; // Set height to auto for actual size
+                img.style.transform = 'scaleX(-1)'; // Mirror horizontally
+                img.style.filter = `invert(100%) contrast(${document.getElementById('contrast').value}%)`; // Color invert and apply contrast
+                td.appendChild(img);
+            } else {
+                td.textContent = cell;
+            }
+
+            tr.appendChild(td);
+        });
+
+        tableBody.appendChild(tr);
+    });
+
+    updateTotalCount(data.length);
+}
+
+function updateImageContrast(contrastValue) {
+    const images = document.querySelectorAll('td img');
+    images.forEach(img => {
+        img.style.filter = `invert(100%) contrast(${contrastValue}%)`;
+    });
+}
+
+function updateTotalCount(count) {
+    const totalCountElement = document.getElementById('totalCount');
+    totalCountElement.textContent = `Total Count: ${count}`;
+}
+
+function sortTable(columnIndex) {
+    if (currentSortColumn === columnIndex) {
+        // If the same column is clicked again, reverse the sorting order
+        isAscending = !isAscending;
+    } else {
+        // If a new column is clicked, set the sorting order to ascending
+        isAscending = true;
+        currentSortColumn = columnIndex;
+    }
+
+    data.sort((a, b) => {
+        const valueA = a[currentSortColumn];
+        const valueB = b[currentSortColumn];
+
+        // Customize the comparison logic based on the data type (string, number, etc.)
+        return isAscending ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+    });
+
+    renderTable(data);
+}
